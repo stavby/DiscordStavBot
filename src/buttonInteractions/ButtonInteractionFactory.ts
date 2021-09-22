1bd21fb0d5acc9d0c5053930975d7e05af27d9aa
oriyafe1 <ori987yafe@gmail.com>
2021-09-22 12:51:39 +0300

    Renaming buttonInteractions folder

--- src/buttonInteractions/ButtonInteractionFactory.ts
+++ src/buttonInteractions/ButtonInteractionFactory.ts
@@ -1,12 +1,16 @@
-import { ButtonInteraction, Interaction } from 'discord.js';
+import { Interaction } from 'discord.js';
 import { InteractionDoesntExistError } from '../errors/InteractionDoesntExistError';
+import { ButtonInteraction } from './ButtonInteraction';
+import { PlayInteraction } from './PlayInteraction';
 
 const buttonInterctions: {
     [interactionName: string]: new (
         args: string[],
         interaction: Interaction
     ) => ButtonInteraction;
-} = {};
+} = {
+    play: PlayInteraction,
+};
 
 const isInteractionExists = (interactionName: string) =>
     Object.keys(buttonInterctions).includes(interactionName);
